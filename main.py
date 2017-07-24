219cdb2078da891830b21022103fffd0fe74c0a0
lucasvieirawerner <lucas.werner02@gmail.com>
2017-07-24 14:06:01 -0300

    if not

--- main.py
+++ main.py
@@ -30,14 +30,18 @@ def getcnpj( nome_restaurante ):
    page = requests.get(busca)
    soup = BeautifulSoup(page.content, 'html.parser')
    resultato_html = soup.find_all(id="search_results")
+   if not resultato_html:
+      return "27691471000140"
    resultato_html_str = str(resultato_html[0])
    strv = resultato_html_str.split('<h3><a href="/', 1000000)
-   link = strv[1].split('" title="041', 1000000)
+   link = strv[1].split('" title=', 1000000)
    link = "http://www.cnpj.ninja/" + link[0]
    # busca do cnpj
    pagina = requests.get(link)
    soupe = BeautifulSoup(pagina.content, 'html.parser')
    html_resultado = soupe.find_all(id="details")
+   if not html_resultado:
+       return "27691471000140"
    html_resultado_str = str(html_resultado[0])
    str_v = html_resultado_str.split('ero do CNPJ</h5><p>', 1000000)
    cnpj = str_v[1].split('</p><div class="adg"', 1000000)
@@ -58,7 +62,7 @@ for x in range(3, 14180):
    cnpj = getcnpj(nome_empresarial)
    telefone = gettelefone(cnpj)
    email = getemail(cnpj)
-   print "email: "+email+" | telefone: "+telefone
+   print "nome: "+nome_empresarial+" | email: "+email+" | telefone: "+telefone
 
 
 
